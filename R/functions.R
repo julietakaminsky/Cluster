9e40903fa0198ee38ceb4d27b9d149a18fe2411e
julietakaminsky <kaminsky.julieta@gmail.com>
2018-08-25 17:05:27 -0300

    function

--- R/functions.R
+++ R/functions.R
@@ -23,21 +23,3 @@ Cluster <- function(epifauna)
   H <- plot(den2, xlab = "Stations", ylab = "Bray-Curtis dissimilarity", nodePar = list(pch = c(1, NA), lab.cex = 0.7))
   return(H)
 }
-
-
-# epifauna <- data.frame(epifauna[,2:14], row.names = epifauna$Station)
-# 'Station' como aparte de la tabla
-# Chequear si 2:14 se podría reemplazar por algo tipo i:n para decirle que tome desde la segunda hasta el final de los datos
-
-
-# epifauna.hell <- decostand(epifauna, "hellinger")
-# Transformación de Hellinger
-
-# epifauna.hell.bc <- vegdist(epifauna.hell, method = "bray")
-# Bray Curtis
-
-# clus2 <- hclust(epifauna.hell.bc, method = "ward.D2")
-# den2 <- as.dendrogram(clus2)
-# plot(den2, xlab = "Stations", ylab = "Bray-Curtis dissimilarity", nodePar = list(pch = c(1, NA), lab.cex = 0.7))
-# return(plot)
-# Cluster "Ward's"
